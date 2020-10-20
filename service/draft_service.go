6d1eb7bb5523b0e11bcef038408527c691504a59
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-10-20 22:54:15 +0530

    [Harish] : Add service layer for upsert tagline for the given draft

--- service/draft_service.go
+++ service/draft_service.go
@@ -1,21 +1,41 @@
 package service
 
-// mockgen -source=service/draft_service.go -destination=mocks/mock_draft_service.go -package=mocks
+//go:generate mockgen -source=draft_service.go -destination=./../mocks/mock_draft_service.go -package=mocks
+
 import (
 	"context"
 	"github.com/gola-glitch/gola-utils/golaerror"
 	"github.com/gola-glitch/gola-utils/logging"
 	"post-api/constants"
 	"post-api/models"
+	"post-api/models/request"
 	"post-api/repository"
 )
 
 type DraftService interface {
 	SaveDraft(postData models.UpsertDraft, ctx context.Context) *golaerror.Error
+	UpsertTagline(taglineRequest request.TaglineSaveRequest, ctx context.Context) *golaerror.Error
 }
 
 type draftService struct {
 	draftRepository repository.DraftRepository
+}
+
+func (service draftService) UpsertTagline(taglineRequest request.TaglineSaveRequest, ctx context.Context) *golaerror.Error {
+	logger := logging.GetLogger(ctx).WithField("class", "DraftService").WithField("method", "UpsertTagline")
+
+	logger.Info("Calling service to save tagline for draft")
+
+	err := service.draftRepository.SaveTaglineToDraft(taglineRequest, ctx)
+
+	if err != nil {
+		logger.Errorf("Error occurred while inserting tagline in draft repository %v", err)
+		return &constants.PostServiceFailureError
+	}
+
+	logger.Info("Successfully stored draft tagline")
+
+	return nil
 }
 
 func (service draftService) SaveDraft(postData models.UpsertDraft, ctx context.Context) *golaerror.Error {
--- service/draft_service_test.go
+++ service/draft_service_test.go
@@ -6,8 +6,10 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/jmoiron/sqlx/types"
 	"github.com/stretchr/testify/suite"
+	"post-api/constants"
 	"post-api/mocks"
 	"post-api/models"
+	"post-api/models/request"
 	"testing"
 )
 
@@ -100,4 +102,34 @@ func (suite *DraftServiceTest) TestSaveDraft_WhenDraftRepositoryReturnsErrorOnTi
 	expectedError := suite.draftService.SaveDraft(newDraft, suite.goContext)
 
 	suite.NotNil(expectedError)
+}
+
+func (suite *DraftServiceTest) TestUpsertTagline_WhenDraftRepositoryReturnsNoError() {
+	saveRequest := request.TaglineSaveRequest{
+		UserID:  "1",
+		DraftID: "dummy-id",
+		Tagline: "this is some tagline",
+	}
+
+	suite.mockDraftRepository.EXPECT().SaveTaglineToDraft(saveRequest, suite.goContext).Return(nil).Times(1)
+
+	expectedError := suite.draftService.UpsertTagline(saveRequest, suite.goContext)
+
+	suite.Nil(expectedError)
+}
+
+
+func (suite *DraftServiceTest) TestUpsertTagline_WhenDraftRepositoryReturnsError() {
+	saveRequest := request.TaglineSaveRequest{
+		UserID:  "1",
+		DraftID: "dummy-id",
+		Tagline: "this is some tagline",
+	}
+
+	suite.mockDraftRepository.EXPECT().SaveTaglineToDraft(saveRequest, suite.goContext).Return(errors.New("something went wrong")).Times(1)
+
+	expectedError := suite.draftService.UpsertTagline(saveRequest, suite.goContext)
+
+	suite.NotNil(expectedError)
+	suite.Equal(&constants.PostServiceFailureError, expectedError)
 }
