977ff37e1477d9ec261fc34d2d0beed55e1ddbdd
Hariharan Sivakumar <harishsivakumar97@gmail.com>
2020-11-01 15:35:48 +0530

    [Harish] : Add create user script for dev environment

--- sql/create_user_dev.sh
+++ sql/create_user_dev.sh
@@ -1,1 +1,1 @@
-psql -h 173.212.235.49 -p 31462 -USYS -d postgres -a -f sql/create_user.sql
+psql -h gola-dev-db.cfmrudiius8m.ap-south-1.rds.amazonaws.com -p 5432 -USYS -d postgres -a -f sql/create_user_dev.sql
